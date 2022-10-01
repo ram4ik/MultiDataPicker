d257c716122128d47daa69917faaa3d1e1b23d63
Ramil <ram4ik@hotmail.com>
2022-10-01 19:03:19 +0300

    Initial Commit

---
+++ MultiDataPicker/MultiDataPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiDataPickerApp.swift
+//  MultiDataPicker
+//
+//  Created by Ramill Ibragimov on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiDataPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
